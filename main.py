43ea8e5a4cee6bb6f606e45ffef6c6729c066b4a
lfgberg <lfgaberg@gmail.com>
2025-02-09 18:46:53 -0500

    fix github search tool

--- main.py
+++ main.py
@@ -1,12 +1,60 @@
 import os
+import requests
 from dotenv import load_dotenv
-from smolagents import CodeAgent, LiteLLMModel, HfApiModel, DuckDuckGoSearchTool
-from github import GitHubSearchTool
+from smolagents import CodeAgent, HfApiModel, DuckDuckGoSearchTool, Tool
+
+
+class GitHubSearchTool(Tool):
+    # Smolagents tool info
+    name = "github_search"
+    description = """
+    This is a tool that uses the GitHub API to submit a search. It returns the repositories which have results for the submitted query."""
+    inputs = {
+        "query": {
+            "type": "string",
+            "description": "The search query to submit to github (A dork such as 'filename:wp-config.php')",
+        }
+    }
+    output_type = "string"
+
+    def query_github(self, query: str):
+        url = "https://api.github.com/search/code"
+
+        headers = {
+            "Authorization": f"Bearer {github_api_token}",
+        }
+
+        params = {"q": query}
+
+        response = requests.get(url, headers=headers, params=params)
+
+        return response
+
+    def parse_response(self, response: requests.Response):
+        results = response.json()
+        output = []
+
+        for item in results["items"]:
+            output.append(
+                [item["repository"]["full_name"], item["path"], item["html_url"]]
+            )
+
+        return output
+
+    # The inference code to be executed
+    def forward(self, query: str):
+        response = self.query_github(query)
+        results = self.parse_response(response)
+        return results
 
 
 def main():
-    # Pull in API key from .env
+    # Pull in API keys from .env
     load_dotenv()
+
+    global hf_api_token
+    global github_api_token
+
     hf_api_token = os.getenv("HF_API_TOKEN")
     github_api_token = os.getenv("GITHUB_API_TOKEN")
 
@@ -31,7 +79,7 @@ def main():
     # GitHub Search Agent
     managed_github_agent = CodeAgent(
         model=model,
-        tools=[GitHubSearchTool(github_api_token)],
+        tools=[GitHubSearchTool()],
         name="github_search",
         description="Runs GitHub searches for you. Give it your query as an argument.",
     )
